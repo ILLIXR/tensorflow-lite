2cb29b118b811ab4d4e6efcddb3468f90ad75944
astro-friedel <friedel@illinois.edu>
2024-06-13 14:43:07 -0500

    updated version of absl

--- src/tools/cmake/modules/abseil-cpp.cmake
+++ src/tools/cmake/modules/abseil-cpp.cmake
@@ -24,7 +24,7 @@ OverridableFetchContent_Declare(
   abseil-cpp
   GIT_REPOSITORY https://github.com/abseil/abseil-cpp
   # Sync with tensorflow/third_party/absl/workspace.bzl
-  GIT_TAG fb3621f4f897824c0dbe0615fa94543df6192f30
+  GIT_TAG 9687a8ea750bfcddf790372093245a1d041b21a3
   GIT_SHALLOW TRUE
   GIT_PROGRESS TRUE
   PREFIX "${CMAKE_BINARY_DIR}"
